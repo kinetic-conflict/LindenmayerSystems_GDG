c2a3e65b2cfadd8c8833bcdad6be7b00af27603b
kinetic-conflict <shreeramta.official@gmail.com>
2026-01-15 14:54:04 +0530

    Importing required modules

---
+++ lsystem_gdg.py
@@ -0,0 +1,8 @@
+import tkinter as tk # for GUI
+import turtle # for Drawing
+
+'''
+F: Move turtle forward.
++: Rotate turtle right by the specified Angle.
+-: Rotate turtle left by the specified Angle.
+'''
