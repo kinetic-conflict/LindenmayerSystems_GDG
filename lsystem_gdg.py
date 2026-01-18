0ad14ca7e6b07c8c94d0c76f57a3cbe25956f64a
kinetic-conflict <shreeramta.official@gmail.com>
2026-01-18 17:51:27 +0530

    GUI Window

--- lsystem_gdg.py
+++ lsystem_gdg.py
@@ -6,3 +6,13 @@ F: Move turtle forward.
 +: Rotate turtle right by the specified Angle.
 -: Rotate turtle left by the specified Angle.
 '''
+
+def main():
+    root = tk.Tk()
+    root.title("Lindenmayer System")
+    root.geometry("900x600")
+    root.mainloop()
+    
+    
+if __name__ == "__main__":
+    main()
