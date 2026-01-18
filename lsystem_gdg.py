63201fdb64e65d0e3efa30e4e425a87e07440559
kinetic-conflict <shreeramta.official@gmail.com>
2026-01-18 17:59:29 +0530

    test button addition

--- lsystem_gdg.py
+++ lsystem_gdg.py
@@ -7,12 +7,23 @@ F: Move turtle forward.
 -: Rotate turtle left by the specified Angle.
 '''
 
+'''
 def main():
     root = tk.Tk()
     root.title("Lindenmayer System")
     root.geometry("900x600")
     root.mainloop()
+'''   
+    # buttons
+def foo():
+    print("Lindenmayer")
+    
+root = tk.Tk()
+root.title("Lindenmayer System")
+root.geometry("900x600")
+CA = tk.Button(root, text="Shree", command = foo )
+CA.pack()
+root.mainloop()
     
     
-if __name__ == "__main__":
-    main()
+foo()
